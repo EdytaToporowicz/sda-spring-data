3a06b3d21275314668637c9aeddc45e62bb52c61
edyta <edyta.toporowicz@gmail.com>
2021-01-21 19:31:23 +0100

    JPA cd update

--- livecodingDB.sql
+++ livecodingDB.sql
@@ -1,3 +1,4 @@
 CREATE database livecoding;
 SELECT * FROM livecoding.blog_posts;
-
+create database files;
+SELECT * FROM files.files_data;
--- src/main/java/pl/sda/blogservicedata/rest/BlogPostController.java
+++ src/main/java/pl/sda/blogservicedata/rest/BlogPostController.java
@@ -1,5 +1,6 @@
 package pl.sda.blogservicedata.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class BlogPostController {
 
     private final BlogPostService blogPostService;
 
+    @Autowired
     public BlogPostController(BlogPostService blogPostService) {
         this.blogPostService = blogPostService;
     }
